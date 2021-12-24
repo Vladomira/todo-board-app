66eccf661c721f74421b0c8b3a38b55b3794b26d
Vladomira <juliavladomira@gmail.com>
2021-12-24 20:50:45 +0300

    control

--- src/redux/todos/todos-reducer.js
+++ src/redux/todos/todos-reducer.js
@@ -19,16 +19,3 @@ export default combineReducers({
   users,
   todos,
 })
-// const loading = createReducer(false, {
-//   [fetchTodos.pending]: () => true,
-//   [fetchTodos.fulfilled]: () => false,
-//   [fetchTodos.rejected]: () => false,
-
-//   [fetchUsers.pending]: () => true,
-//   [fetchUsers.fulfilled]: () => false,
-//   [fetchUsers.rejected]: () => false,
-
-//   [toggleCompleted.pending]: () => true,
-//   [toggleCompleted.fulfilled]: () => false,
-//   [toggleCompleted.rejected]: () => false,
-// })
